9952f723d37657fb822c80789d19f9f93913996b
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-08-11 12:02:45 +0100

    Using become() to switch status

--- bb.scala
+++ bb.scala
@@ -2,8 +2,8 @@ import akka.actor.{ ActorRef, ActorSystem, Props, Actor, Inbox, Scheduler }
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 
-case object TryProduce
-case object TryConsume
+case object Produce
+case object Consume
 case object Get
 case class Put(v: Int)
 case class Success(v: Int)
@@ -13,6 +13,7 @@ case object WakeUp
 
 final class BoundedBuffer(capacity: Int) extends Actor {
   import Utils._
+  import context._
 
   implicit val color = Console.GREEN
   private val who = s"Buffer($capacity)"
@@ -26,31 +27,61 @@ final class BoundedBuffer(capacity: Int) extends Actor {
   private val waitingProducers = scala.collection.mutable.ListBuffer.empty[ActorRef]
   private val waitingConsumers = scala.collection.mutable.ListBuffer.empty[ActorRef]
 
-  def receive = {
-    case Put(v) if (!amIFull) => {
+  def available: Receive = {
+    case Get => {
+      val value = fetch()
+      say(who, s"Fetched value: $value, count: $count")
+      if (amIEmpty) {
+        say(who, "Switching to EMPTY state")
+        become(empty)
+      }
+      sender ! Success(value)
+    }
+    case Put(v) => {
       store(v)
       say(who, s"Stored value: $v, count: $count")
-      awakeConsumers
+      if (amIFull) {
+        say(who, "Switching to FULL state")
+        become(full)
+      }
       sender ! Success(v)
     }
-    case Put(v) if (amIFull) => {
+    case msg => say(who, s"Incorrect message for my status (AVAILABLE): $msg", true)
+  }
+
+  def full: Receive = {
+    case Put(v) => {
       say(who, s"Full buffer for value: $v, count: $count")
       waitingProducers += sender
       sender ! Full
     }
-    case Get if (!amIEmpty) => {
+    case Get => {
       val value = fetch()
       say(who, s"Fetched value: $value, count: $count")
-      awakeProducers
+      awakeProducers()
+      become(available)
       sender ! Success(value)
     }
-    case Get if (amIEmpty) => {
+    case msg => say(who, s"Incorrect message for my status (FULL): $msg", true)
+  }
+
+  def empty: Receive = {
+    case Get => {
       say(who, s"Empty buffer, count: $count")
       waitingConsumers += sender
       sender ! Empty
     }
-    case msg => say(who, s"Unrecognized message: $msg")
+    case Put(v) => {
+      store(v)
+      say(who, s"Stored value: $v, count: $count")
+      awakeConsumers()
+      become(available)
+      sender ! Success(v)
+    }
+    case msg => say(who, s"Incorrect message for my status (EMPTY): $msg", true)
   }
+
+  override def receive = empty   // initially the buffer is empty
 
   private def store(v: Int) {
     buffer(count) = v
@@ -92,80 +123,94 @@ final class BoundedBuffer(capacity: Int) extends Actor {
 
 final class Producer(id: Int, buffer: ActorRef, scheduler: Scheduler) extends Actor {
   import Utils._
+  import context._
 
   implicit val color = Console.RED
   private val who = s"Producer-$id"
 
-  private[this] var sleeping = false
-  private[this] var waiting = false
-
   say(who, s"Starting Producer with id: $id, buffer is $buffer")
 
-  override def receive = {
-    case TryProduce if (!waiting && !sleeping) => {
+  def producing: Receive = {
+    case Produce => {
       val value = produce()
       say(who, s"Trying to produce value: $value ...")
-      waiting = true
+      become(waiting)
       buffer ! Put(value)
     }
-    case Full if (waiting) => {
-      say(who, "Got a Full from the Buffer. Going to sleep...")
-      waiting = false
-      sleeping = true
-    }
-    case Success(v) if (waiting) => {
-      say(who, s"Successfully produced value: $v")
-      waiting = false
-      scheduler.scheduleOnce(scala.math.abs(scala.util.Random.nextInt(500)) milliseconds) {
-        self ! TryProduce
-      }
-    }
-    case WakeUp if (sleeping) => {
-      say(who, "Got a WakeUp from Buffer. Trying to produce ...")
-      sleeping = false
-      self ! TryProduce
-    }
-    case msg => say(who, s"Got $msg while sleeping: $sleeping or waiting: $waiting. Ignoring...", true)
+    case msg => say(who, s"Incorrect message for my status (PRODUCING): $msg", true)
   }
+
+  def waiting: Receive = {
+    case Success(v) => {
+      say(who, s"Successfully produced value: $v")
+      become(producing)
+      scheduler.scheduleOnce(scala.math.abs(scala.util.Random.nextInt(500)) milliseconds) {
+        self ! Produce
+      }(scala.concurrent.ExecutionContext.Implicits.global)
+    }
+    case Full => {
+      say(who, "Got a Full from the Buffer. Going to sleep...")
+      become(sleeping)
+    }
+    case msg => say(who, s"Incorrect message for my status (WAITING): $msg", true)
+  }
+
+  def sleeping: Receive = {
+    case WakeUp => {
+      say(who, "Got a WakeUp from Buffer. Trying to produce ...")
+      become(producing)
+      self ! Produce
+    }
+    case msg => say(who, s"Incorrect message for my status (SLEEPING): $msg", true)
+  }
+
+  override def receive = producing
 
   private def produce(): Int = scala.math.abs(scala.util.Random.nextInt(1000))
 }
 
 final class Consumer(id: Int, buffer: ActorRef, scheduler: Scheduler) extends Actor {
   import Utils._
-
-  private[this] var sleeping = false
-  private[this] var waiting = false
+  import context._
 
   implicit val color = Console.YELLOW
   private val who = s"Consumer-$id"
   say(who, s"Starting Consumer with id: $id, buffer is $buffer")
 
-  override def receive = {
-    case TryConsume if (!waiting && !sleeping) => {
+  def consuming: Receive = {
+    case Consume => {
       say(who, "Trying to consume ...")
       buffer ! Get
-      waiting = true
+      become(waiting)
     }
-    case Empty if (waiting) => {
-      say(who, "Buffer is empty. Going to sleep ...")
-      waiting = false
-      sleeping = true
-    }
-    case Success(v) if (waiting) => {
-      say(who, s"Successfully consumed value: $v")
-      waiting = false
-      scheduler.scheduleOnce(scala.math.abs(scala.util.Random.nextInt(500)) milliseconds) {
-        self ! TryConsume
-      }
-    }
-    case WakeUp if (sleeping) => {
-      say(who, "Got a WakeUp from Buffer. Trying to consume ...")
-      sleeping = false
-      self ! TryConsume
-    }
-    case msg => say(who, s"Got $msg while sleeping: $sleeping or waiting: $waiting. Ignoring...", true)
+    case msg => say(who, s"Incorrect message for my status (CONSUMING): $msg", true)
   }
+
+  def waiting: Receive = {
+    case Success(v) => {
+      say(who, s"Successfully consumed value: $v")
+      become(consuming)
+      scheduler.scheduleOnce(scala.math.abs(scala.util.Random.nextInt(500)) milliseconds) {
+        self ! Consume
+      }(scala.concurrent.ExecutionContext.Implicits.global)
+    }
+    case Empty => {
+      say(who, "Buffer is empty. Going to sleep ...")
+      become(sleeping)
+    }
+    case msg => say(who, s"Incorrect message for my status (WAITING): $msg", true)
+  }
+
+  def sleeping: Receive = {
+    case WakeUp => {
+      say(who, "Got a WakeUp from Buffer. Trying to consume ...")
+      become(consuming)
+      self ! Consume
+    }
+    case msg => say(who, s"Incorrect message for my status (SLEEPING): $msg", true)
+  }
+
+  override def receive = consuming
 }
 
 object Utils {
@@ -174,11 +219,10 @@ object Utils {
   }
 }
 
-
 object BB extends App {
-  val producers = 2
-  val consumers = 4
-  val capacity = 10
+  val producers = 10
+  val consumers = 12
+  val capacity = 8
   println(s"Starting Bounded Buffer (producers: $producers, consumers: $consumers, capacity: $capacity)")
 
   val system = ActorSystem("bounded-buffer") // Create the 'bounded-buffer' system
@@ -189,10 +233,10 @@ object BB extends App {
 
   for (i <- 1 to producers) {
     val p = system.actorOf(Props(classOf[Producer], i, boundedBuffer, scheduler), "producer-" + i)
-    inbox.send(p, TryProduce)
+    inbox.send(p, Produce)
   }
   for (i <- 1 to consumers) {
     val c = system.actorOf(Props(classOf[Consumer], i, boundedBuffer, scheduler), "consumer-" + i)
-    inbox.send(c, TryConsume)
+    inbox.send(c, Consume)
   }
 }
